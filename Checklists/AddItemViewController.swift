e26aa3498203159a63570dc39322be0e8c2530c6
Ed <edmccormic@gmail.com>
2017-03-16 11:34:36 -0600

    first commit

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Ed McCormic on 3/16/17.
+//  Copyright © 2017 Swiftbeard. All rights reserved.
+//
+
+import Foundation
---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,9 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Ed McCormic on 3/7/17.
+//  Copyright © 2017 Swiftbeard. All rights reserved.
+//
+
+import Foundation
