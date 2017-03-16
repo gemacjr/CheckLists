b718074f6426389740dc89e6938dfbfd2782f506
Ed <edmccormic@gmail.com>
2017-03-16 11:34:56 -0600

    first commit

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+import UIKit
+
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    @IBAction func cancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func done() {
+        
+        print("Contents of the text field: \(textField!)")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        
+        let oldText = textField.text! as NSString
+        let newText = oldText.replacingCharacters(in: range, with: string) as NSString
+        
+        if newText.length > 0 {
+            doneBarButton.isEnabled = true
+        } else {
+            doneBarButton.isEnabled = false
+        }
+        
+        return true
+    }
+}
